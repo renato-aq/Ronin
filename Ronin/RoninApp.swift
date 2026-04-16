0df04edcffab74824e3f2f6d4e3c54631791f419
Renato Aquino <renato.silva.aquino12@gmail.com>
2026-04-16 19:51:46 -0300

    Initial Commit

---
+++ Ronin/RoninApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoninApp.swift
+//  Ronin
+//
+//  Created by Renato Silva de Aquino on 16/04/26.
+//
+
+import SwiftUI
+
+@main
+struct RoninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
